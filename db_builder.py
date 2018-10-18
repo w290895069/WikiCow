cb71faf0d57a262c62a05f8e466c796869167c75
Clara Mohri <claramohri@napoleon.local>
2018-10-18 12:56:22 -0400

    Hard coded db

--- db_builder.py
+++ db_builder.py
@@ -13,10 +13,25 @@ c = db.cursor()
 command = "CREATE TABLE users (username TEXT, password TEXT)"
 c.execute(command)
 
+#some hard coded values for now
+params = [('clara', 'pass'), ('jiajie', 'pass1'), ('william', 'pass2'), ('alex', 'pass0')]
+for i in params:
+    c.execute('INSERT INTO users VALUES(?, ?)', i)
+
+
 #create table for all of the stories and their entries
 command = "CREATE TABLE stories (story TEXT, contributer TEXT, timestamp TEXT, contribution TEXT)"
 c.execute(command)
 
+#some hard coded values for now
+params = [('title', 'clara', '001', 'once upon a time'),
+          ('title', 'jiajie', '010', 'there was a family'),
+          ('title', 'william', '011', 'that lived in America.'),
+          ('title1', 'alex', '001', 'The sky was blue,')]
+
+for i in params:
+    c.execute('INSERT INTO stories VALUES(?, ?, ?, ?)', i)
+    
 #make changes to db and then closer
 db.commit()
 db.close()
